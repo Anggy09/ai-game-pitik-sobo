94d293c5ac5550cd14656fe30357d566f75f951e
Anggy09 <33244030+Anggy09@users.noreply.github.com>
2017-10-31 11:33:16 +0700

    Update Splash.java

--- Splash.java
+++ Splash.java
@@ -1,11 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+hor (your name) 
 
-/**
- * Write a description of class Splash here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Splash extends World
 {
 
